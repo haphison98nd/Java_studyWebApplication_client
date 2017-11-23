3f58af173092c07564a1b43084463008d299767b
kotori <>
2017-11-23 22:59:26 +0900

    change network done

--- main.java
+++ main.java
@@ -1,26 +1,15 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.*;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.Iterator;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.net.SocketException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
+import java.io.DataInputStream;
+import java.io.OutputStream;
+import java.io.DataOutputStream;
 
 class JavaClient{
 	public static void main(String args[]){
 
         JavaTCP jtcp=new JavaTCP();
         //Java_myBaseSystem jmyBase=new Java_myBaseSystem();
-        String str=String.valueOf(10);
         System.out.println
         (
             jtcp.tcp_text(jtcp.loadProfile_tcpInformation("profile.txt",""))
@@ -40,13 +29,10 @@ class Java_myBaseSystem{
             String input_data=new String(in.readLine());
             return input_data;
             
-        } catch (Exception e) {
+        }catch (Exception e) {
             return e.toString();
         }
     }
-
-
-
 }
 
 
@@ -59,30 +45,34 @@ class JavaTCP{
         try{
             //文字列を分割（csv方式）
             String splitstr[]=serverInformation.split(",",0);
-
             String host=splitstr[0];
-            int sendport=Integer.parseInt(splitstr[1]);
-            int receport=Integer.parseInt(splitstr[2]);
-            String data=splitstr[3];
+            int port=Integer.parseInt(splitstr[1]);
+            String data=splitstr[2];
+            System.out.println(host+"/"+port+"/"+data);
 
-            System.out.println(host+"/"+sendport+"/"+receport+"/"+data);
+            //InputStreamReader is = new InputStreamReader(data);
+            //BufferedReader br = new BufferedReader(is);
+            
 
             //指定サーバにデータを送る
-			Socket s = new Socket(host, sendport);
-			PrintWriter pw = new PrintWriter(s.getOutputStream());
-			pw.println(data);
-			pw.flush();
-			s.shutdownOutput();
-            s.close();
+			Socket s = new Socket(host, port);
+            OutputStream os = s.getOutputStream();
             
-            //サーバからの応答を返り値に指定する
-			ServerSocket o_s=new ServerSocket(receport);
-			Socket osocket=o_s.accept();
-            BufferedReader br =new BufferedReader(new InputStreamReader(osocket.getInputStream()));
-			String odata = br.readLine();
-			br.close();
-            o_s.close();
-            return odata;
+            InputStream sok_is = s.getInputStream();
+			InputStreamReader sok_isr = new InputStreamReader(sok_is);
+			BufferedReader sok_br = new BufferedReader(sok_isr);
+            
+            //data = br.readLine();	//キー1行入力
+            os.write(data.getBytes());//送信
+            
+            String receive = sok_br.readLine();//受信データ取得
+            //System.out.println("受信『" + receive + "』");
+    
+
+
+            s.shutdownOutput();
+            s.close();
+            return receive;
 
 
 		}
@@ -99,13 +89,12 @@ class JavaTCP{
             //TCPの情報を尋ねる
             String host=myBase.textInfo("input host address");
             String sendport=myBase.textInfo("input host port");
-            String receport=myBase.textInfo("input receport");
             String data;
             if(sendData==""){data=myBase.textInfo("input sendData");}
             else{data=sendData;}
 
             //CSV形式で文字列を生成
-            String ServerInformation=output_csv(host,sendport,receport,data);
+            String ServerInformation=output_csv(host,sendport,data);
 
             return ServerInformation;
             
@@ -116,10 +105,10 @@ class JavaTCP{
     }
 
     //CSV形式で文字列を生成
-    public String output_csv(String host,String sendport,String receport,String data)
+    public String output_csv(String host,String sendport,String data)
     {
         String csvStr;
-        csvStr=host+","+sendport+","+receport+","+data;
+        csvStr=host+","+sendport+","+data;
         return csvStr;
     }
 
@@ -160,6 +149,8 @@ class JavaTCP{
     }
 
     //ファイルチェッカー
-    private boolean checkBeforeReadfile(File file){
-        if (file.exists()){if (file.isFile() && file.canRead()){return true;}}return false;}
+    private boolean checkBeforeReadfile(File file)
+    {
+        if (file.exists()){if (file.isFile() && file.canRead()){return true;}}return false;
+    }
 }
